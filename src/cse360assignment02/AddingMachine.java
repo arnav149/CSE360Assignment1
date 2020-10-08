5fd1be35eb6d677e57da3dc2768965efc7abd4b8
Arnav Kasturia <arnavkasturia@hotmail.com>
2020-10-07 18:28:14 -0700

    second commit

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,54 @@
 package cse360assignment02;
 
-public class AddingMachine {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddingMachine
+{
     private int total;
+    private List<String> list = new ArrayList<>(); //list to keep track of all the operations
 
-    public AddingMachine () {
-        total = 0;  // not needed - included for clarity
+    public AddingMachine ()
+    {
+        total = 0;
+        list.add(total+""); //adding the initial total into the string
     }
 
-    public int getTotal () {
-        return 0;
+    public int getTotal ()
+    {
+        return this.total;
     }
 
-    public void add (int value) {
+    public void add (int value)
+    {
+        list.add(" + "+value); //adding "+" operation along with value to the list
+        this.total += value;
     }
 
-    public void subtract (int value) {
+    public void subtract (int value)
+    {
+        list.add(" - "+value); //adding "-" operation along with value to the list
+        this.total -= value;
     }
 
-    public String toString () {
-        return "";
+    public String toString ()
+    {
+        //building the required string using StringBuilder
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<list.size(); i++)
+        {
+            sb.append(list.get(i));
+        }
+
+        return sb.toString();
     }
 
-    public void clear() {
+    public void clear()
+    {
+        //clearing the AddingMachine and adding 0 to the list to start with
+        this.total = 0;
+        list.clear();
+        list.add(0+"");
     }
 }
